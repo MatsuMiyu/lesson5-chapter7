d33e8779a32f5b8aec3cf2c4cf6e4b70199093ef
MatsuMiyu <gsshinzato@gmail.com>
2025-04-09 23:39:46 +0900

    途中の変更をコミット

---
+++ app.js
@@ -0,0 +1,44 @@
+for(let i = 1; i < 51; i++){
+  if(i == 10) {
+    console.log('今' + i + '回ループしました。');
+
+    if(i % 4 == 0) {
+      console.log('4で割れる数です。' + i)
+    }
+  }
+
+  if(i == 20) {
+    console.log('今' + i + '回ループしました。');
+
+    if(i % 4 ==0) {
+      console.log('4で割れる数です。' + i)
+    }
+  }
+
+    if(i == 30) {
+    console.log('今' + i + '回ループしました。');
+
+    if(i % 4 ==0) {
+      console.log('4で割れる数です。' + i)
+    }
+  }
+
+    if(i == 40) {
+    console.log('今' + i + '回ループしました。');
+
+    if(i % 4 ==0) {
+      console.log('4で割れる数です。' + i)
+    }
+  }
+
+    if(i == 50) {
+    console.log('今' + i + '回ループしました。');
+    alert('50回カウントが終わりました。')
+    
+    if(i % 4 ==0) {
+      console.log('4で割れる数です。' + i)
+    }
+  }
+}
+
+
