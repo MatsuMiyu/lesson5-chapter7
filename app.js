8f3d671b319491de67acf87f34395f00fce8584b
MatsuMiyu <gsshinzato@gmail.com>
2025-04-10 00:09:01 +0900

    課題提出_2回目

--- app.js
+++ app.js
@@ -1,44 +1,42 @@
-for(let i = 1; i < 51; i++){
-  if(i == 10) {
+for(let i = 1; i <= 50; i++) {
+  if (i % 10 == 0) {
     console.log('今' + i + '回ループしました。');
-
-    if(i % 4 == 0) {
-      console.log('4で割れる数です。' + i)
-    }
   }
 
-  if(i == 20) {
-    console.log('今' + i + '回ループしました。');
-
-    if(i % 4 ==0) {
-      console.log('4で割れる数です。' + i)
-    }
+  if (i % 4 == 0) {
+    console.log('4で割れる数です。' + i);
   }
 
-    if(i == 30) {
+  if (i % 20 == 0) {
     console.log('今' + i + '回ループしました。');
-
-    if(i % 4 ==0) {
-      console.log('4で割れる数です。' + i)
-    }
   }
 
-    if(i == 40) {
-    console.log('今' + i + '回ループしました。');
-
-    if(i % 4 ==0) {
-      console.log('4で割れる数です。' + i)
-    }
+  if (i % 4 == 0) {
+    console.log('4で割れる数です。' + i);
   }
 
-    if(i == 50) {
+  if (i % 30 == 0) {
     console.log('今' + i + '回ループしました。');
-    alert('50回カウントが終わりました。')
-    
-    if(i % 4 ==0) {
-      console.log('4で割れる数です。' + i)
-    }
+  }
+
+  if (i % 4 == 0) {
+    console.log('4で割れる数です。' + i);
+  }
+
+  if (i % 40 == 0) {
+    console.log('今' + i + '回ループしました。');
+  }
+
+  if (i % 4 == 0) {
+    console.log('4で割れる数です。' + i);
+  }
+
+  if (i % 50 == 0) {
+    console.log('今' + i + '回ループしました。');
+  }
+
+  if (i % 4 == 0) {
+    console.log('4で割れる数です。' + i);
+    alert('50回カウントが終わりました。');
   }
 }
-
-
